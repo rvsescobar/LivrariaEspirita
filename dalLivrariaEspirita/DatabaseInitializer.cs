e0fc458275aa6e5e4f443eb02eda5a88ab8a7173
rvsescobar <rvsescobar@gmail.com>
2026-03-31 18:00:11 -0300

    Inclusão dos método CreateTable() e CreateTableEditoras()

--- dalLivrariaEspirita/DatabaseInitializer.cs
+++ dalLivrariaEspirita/DatabaseInitializer.cs
@@ -7,13 +7,41 @@ namespace dalLivrariaEspirita
     /// </summary>
     public static class DatabaseInitializer
     {
+
+        /// <summary>
+        /// String de conexão com o banco SQLite.
+        /// O readonly significa que esse valor não pode ser alterado depois de inicializado.
+        /// </summary>
+        private static readonly string _connectionString = "Data Source=DbLivraria.db";
         /// <summary>
         /// Método privado e estático para criação e conexão com o banco e dados.
         /// </summary>
         private static void CreateDb()
         {
-            using var conn = new SqliteConnection("Data Source=DbLivraria.db");
+            using var conn = new SqliteConnection(_connectionString);
             conn.Open();
+        }
+        /// <summary>
+        /// Método genérico para criação de tabela.
+        /// </summary>
+        /// <param name="sqlCreateTable">SQL específico da criação da tabela.</param>
+        private static void CreateTable(string sqlCreateTable)
+        {
+            using var conn = new SqliteConnection(_connectionString);
+            conn.Open();
+
+            var cmd = conn.CreateCommand();
+            cmd.CommandText = sqlCreateTable;
+            cmd.ExecuteNonQuery();
+        }
+        /// <summary>
+        /// Método para criação da tabela Editoras.
+        /// </summary>
+        private static void CreateTableEditoras()
+        {
+            CreateTable(@"CREATE TABLE IF NOT EXISTS Editoras (
+                            Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                            Nome TEXT NOT NULL);");
         }
         /// <summary>
         /// Método público e estático que inicializa o banco.
@@ -23,6 +51,7 @@ namespace dalLivrariaEspirita
         public static void Initialize()
         {
             CreateDb();
+            CreateTableEditoras();
         }
     }
 }
